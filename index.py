b5a4d6bf6e48f319029cd84b6c0d02e41c8e8d9d
tanvird3 <tanvir.d3@gmail.com>
2021-07-17 02:04:23 +0600

    feature update

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import investpy
 
-def index_hist(startdate, enddate): 
-  df = investpy.get_index_historical_data(index='DSE Broad', country='Bangladesh', from_date=startdate, to_date=enddate)
+def index_hist(startdate, enddate, index): 
+  df = investpy.get_index_historical_data(index=index, country='Bangladesh', from_date=startdate, to_date=enddate)
   df = df.reset_index().sort_values(by="Date", ascending=False)
   return (df)
 
